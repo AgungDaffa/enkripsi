56c6ace84f7e17e58d777e8eb34b0336709e59d1
Tifa <120162380+TifaAmal1na@users.noreply.github.com>
2024-08-15 11:02:48 +0700

    sidebar
    menambahkan sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload File</title>
-</head>
-<body>
-    <form action="/upload" method="POST" enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="file">
-        <button type="submit">Upload</button>
-    </form>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Upload File')
+
+@section('content')
+<form action="/upload" method="POST" enctype="multipart/form-data">
+    @csrf
+    <input type="file" name="file">
+    <button type="submit">Upload</button>
+</form>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-light border-right" id="sidebar-wrapper">
+    <div class="sidebar-heading">File Security</div>
+    <div class="list-group list-group-flush">
+        <a href="{{ route('index') }}" class="list-group-item list-group-item-action bg-light">Upload File</a>
+        <a href="{{ route('uploads.list') }}">List of Uploaded Files</a>
+    </div>
+</div>
