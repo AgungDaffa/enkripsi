f5c7cb851599e2f091a1b18ad30687a7e56df8cc
Ananda Galih Pratiwi <nandaglhp@gmail.com>
2024-08-16 15:12:13 +0700

    validate-form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,31 @@
 @extends('layouts.app')
 
-@section('title', 'Upload File')
-
 @section('content')
-<form action="/upload" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="file">
-    <button type="submit">Upload</button>
-</form>
+    <div class="container mt-5">
+        <h1>Upload File</h1>
+
+        {{-- Menampilkan pesan error --}}
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        {{-- Form untuk upload file --}}
+        <form action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="file">Pilih File:</label>
+                <input type="file" name="file" class="form-control" id="file">
+                @if ($errors->has('file'))
+                    <span class="text-danger">{{ $errors->first('file') }}</span>
+                @endif
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Upload</button>
+        </form>
+    </div>
 @endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,9 @@
 <div class="bg-light border-right" id="sidebar-wrapper">
-    <div class="sidebar-heading">agung</div>
     <div class="list-group list-group-flush">
         <a href="{{ route('index') }}" class="list-group-item list-group-item-action bg-light">Upload File</a>
-        <a href="{{ route('uploads.encryption') }}" class="list-group-item list-group-item-action bg-light">List of Encrypted Files</a>
-        <a href="{{ route('uploads.decryption') }}" class="list-group-item list-group-item-action bg-light">List of Decrypted Files</a>
+        <a href="{{ route('uploads.encryption') }}" class="list-group-item list-group-item-action bg-light">List of
+            Encrypted Files</a>
+        <a href="{{ route('uploads.decryption') }}" class="list-group-item list-group-item-action bg-light">List of
+            Decrypted Files</a>
     </div>
 </div>
